8fb68b612f47056cb8cb2e765db22d923ade8a83
Pablo Arias <pabloarias.ic@gmail.com>
2023-03-13 15:31:22 -0300

    example of text-documentation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+  /*
+  <Link
+    className="button button--secondary button--lg"
+    to="/docs/intro">
+    Docusaurus Tutorial - 00min ⏱️
+  </Link>
+  */
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 00min ⏱️
-          </Link>
-        </div>
+        <div className={styles.buttons}></div>
       </div>
     </header>
   );
@@ -31,7 +32,8 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
@@ -39,5 +41,3 @@ export default function Home() {
     </Layout>
   );
 }
-
-
